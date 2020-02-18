68f884b51506ebde8ed3164604a56ee0c81dc55f
musta7188 <musta7188@hotmail.it>
2020-02-18 13:24:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+    get '/new' do
+      erb :new
+    end
+
+    post '/show' do
+      erb :show
+    end
 
   end
 end
